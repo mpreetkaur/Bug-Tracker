05d5950ea4ad387264a6b0cae9d857ef4cc453ec
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-10-01 14:36:15 -0500

    added classes folder

---
+++ Bug Tracker/Models/Classes/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bug_Tracker.Models.Classes
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set;}
+
+        public ICollection<ApplicationUser> Users { get; set; }
+        
+        public Project()
+        {
+            Users = new HashSet<ApplicationUser>();
+        }
+    }
+}
